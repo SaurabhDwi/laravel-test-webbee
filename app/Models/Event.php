731f34617848f94c9b8b19d09415dd384f53c12b
saurabh dwi <saurabh.dwi82@gmail.com>
2021-05-05 18:59:24 +0530

    Question 2 commit

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
-
+use App\Models\Workshop;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    public function workShops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
 }
--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -1,13 +1,16 @@
 <?php
 
-
 namespace App\Models;
 
-
+use App\Models\Event;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Date;
 
 class Workshop extends Model
 {
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
 
 }
